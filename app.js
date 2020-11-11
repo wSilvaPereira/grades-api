a5d1337feb998df2330dc017a1aee892ac6fcbc8
wsilvapereira <williampereira84@gmail.com>
2020-11-11 00:24:11 -0300

    cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors({ origin: 'http://localhost:3000' }));
 // app.use(
 //   cors({
 //     origin: 'https://wsp-grades-api.herokuapp.com',
